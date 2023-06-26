9ef543ebc845e2f029a18c2c2f4ca4feeab8796c
royx0612 <royx0612@gmail.com>
2023-06-26 21:48:28 +0800

    Update myMemberPage.vue

--- pages/myMemberPage.vue
+++ pages/myMemberPage.vue
@@ -3,18 +3,10 @@
 		<!-- 顯示 user 如果有資訊 -->
 		<div>SSR 已經登入成功</div>
 		<div>你的 email 是 {{ user.email }}</div>
-		<!-- show post -->
-		<div class="flex flex-col spacy-y mt-12">
-			<div class="text-xl">文章列表：</div>
-			<div v-for="post in posts" :key="post.id">
-				> 標題：{{ post.postTitle }}
-			</div>
-		</div>
 	</div>
 </template>
 
 <script setup>
-// import { $larafetch } from '~/utils/$larafetch';
 definePageMeta({ middleware: ['auth'] });
 
 const user = useUser();
